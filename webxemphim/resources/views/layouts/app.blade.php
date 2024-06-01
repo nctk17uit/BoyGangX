45dc33d21e85db3850fd23b815bfeb1695c37f48
MiyukiYuzaku <dnthanhpm@gmail.com>
2024-06-01 18:39:27 +0700

    đổi tên db, đổi route khi bấm public ra home

--- webxemphim/resources/views/layouts/app.blade.php
+++ webxemphim/resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="{{ asset('header/header.css') }}">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Web Phim BoyGangX</title>
