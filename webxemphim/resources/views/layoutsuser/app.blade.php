b49c364774da140aabd906fb2ac8ffb78c52a0d5
dev <dev@gmail.com>
2024-06-03 07:59:36 +0700

    filecoaddphimne

--- webxemphim/resources/views/layoutsuser/app.blade.php
+++ webxemphim/resources/views/layoutsuser/app.blade.php
@@ -30,7 +30,6 @@
         <main>
             @include('layoutsuser.header')
             @yield('content')
-            @include('layoutsuser.footer')
         </main>
     </div> 
     <script type="text/javascript" src=https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js></script>
